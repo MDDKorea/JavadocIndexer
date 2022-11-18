f06b259d98a45295511707f9fae7421e5cb734f1
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2022-11-18 22:06:05 +0100

    examples: Remove META-INF with module definition from jetbrains annotations

--- src/main/resources/examples/fetch_jetbrains_annotations.sh
+++ src/main/resources/examples/fetch_jetbrains_annotations.sh
@@ -2,3 +2,4 @@
 
 curl -L https://repo1.maven.org/maven2/org/jetbrains/annotations/23.0.0/annotations-23.0.0-sources.jar -o /tmp/annotations.zip
 unzip /tmp/annotations.zip -d ../../../../indexes/jetbrains-annotations/
+rm -rf ../../../../indexes/jetbrains-annotations/META-INF
